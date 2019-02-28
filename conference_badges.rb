83af46029e6fe4916fbac3336e894d3c8d646f78
Brian Simonton <bsimonton78@gmail.com>
2019-02-28 16:09:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+
+def badge_maker(name)
+  
+  return "Hello, my name is #{name}."
+ 
+end 
+
+
+
+
+def batch_badge_creator(names)
+  
+ names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"] 
+ 
+  names.each do |name|
+   names << "Hello, my name is #{name}."
+  end
+  
+names
+
+end
+
+
+
+
