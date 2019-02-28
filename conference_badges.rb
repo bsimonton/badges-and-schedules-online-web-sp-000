deb8bcd234132b3cc2f499b0524e858f05974d8e
Brian Simonton <bsimonton78@gmail.com>
2019-02-28 16:14:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,7 +18,15 @@ def batch_badge_creator(names)
 end
   
 
-
+def assign_rooms(names)
+  room=0
+  
+  names.collect do |name|
+    room+=1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+  
+end
 
 
 
